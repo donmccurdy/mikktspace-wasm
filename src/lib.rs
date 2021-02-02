c17a5ee970bbfe5daec7611c25b7e6763fa3e434
Don McCurdy <donmccurdy@google.com>
2021-02-01 20:26:09 -0800

    Use mikktspace crate as a dependency, not copy/pasted.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-mod mikktspace;
-
 use wasm_bindgen::prelude::*;
-use nalgebra::{Vector2, Vector3};
+use mikktspace::{Geometry};
 
 #[cfg(debug_assertions)]
 extern crate console_error_panic_hook;
@@ -10,80 +8,23 @@ extern crate console_error_panic_hook;
 use std::panic;
 
 /******************************************************************************
- * MikkTSpace interface.
+ * JavaScript interface.
  */
 
-/// The interface by which mikktspace interacts with your geometry.
-pub trait Geometry {
-    /// Returns the number of faces.
-    fn num_faces(&self) -> usize;
+ /// Generates vertex tangents for the given position/normal/texcoord attributes.
+#[wasm_bindgen]
+#[allow(non_snake_case)]
+pub fn computeVertexTangents(position: Vec<f32>, normal: Vec<f32>, texcoord: Vec<f32>) -> Vec<f32> {
+    #[cfg(debug_assertions)]
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    /// Returns the number of vertices of a face.
-    fn num_vertices_of_face(&self, face: usize) -> usize;
-
-    /// Returns the position of a vertex.
-    fn position(&self, face: usize, vert: usize) -> [f32; 3];
-
-    /// Returns the normal of a vertex.
-    fn normal(&self, face: usize, vert: usize) -> [f32; 3];
-
-    /// Returns the texture coordinate of a vertex.
-    fn tex_coord(&self, face: usize, vert: usize) -> [f32; 2];
-
-    /// Sets the generated tangent for a vertex.
-    /// Leave this function unimplemented if you are implementing
-    /// `set_tangent_encoded`.
-    fn set_tangent(
-        &mut self,
-        tangent: [f32; 3],
-        _bi_tangent: [f32; 3],
-        _f_mag_s: f32,
-        _f_mag_t: f32,
-        bi_tangent_preserves_orientation: bool,
-        face: usize,
-        vert: usize,
-    ) {
-        let sign = if bi_tangent_preserves_orientation {
-            1.0
-        } else {
-            -1.0
-        };
-        self.set_tangent_encoded([tangent[0], tangent[1], tangent[2], sign], face, vert);
-    }
-
-    /// Sets the generated tangent for a vertex with its bi-tangent encoded as the 'W' (4th)
-    /// component in the tangent. The 'W' component marks if the bi-tangent is flipped. This
-    /// is called by the default implementation of `set_tangent`; therefore, this function will
-    /// not be called by the crate unless `set_tangent` is unimplemented.
-    fn set_tangent_encoded(&mut self, _tangent: [f32; 4], _face: usize, _vert: usize) {}
-}
-
-fn get_position<I: Geometry>(geometry: &mut I, index: usize) -> Vector3<f32> {
-    let (face, vert) = index_to_face_vert(index);
-    geometry.position(face, vert).into()
-}
-
-fn get_tex_coord<I: Geometry>(geometry: &mut I, index: usize) -> Vector3<f32> {
-    let (face, vert) = index_to_face_vert(index);
-    let tex_coord: Vector2<f32> = geometry.tex_coord(face, vert).into();
-    tex_coord.insert_row(2, 1.0)
-}
-
-fn get_normal<I: Geometry>(geometry: &mut I, index: usize) -> Vector3<f32> {
-    let (face, vert) = index_to_face_vert(index);
-    geometry.normal(face, vert).into()
-}
-
-fn index_to_face_vert(index: usize) -> (usize, usize) {
-    (index >> 2, index & 0x3)
-}
-
-fn face_vert_to_index(face: usize, vert: usize) -> usize {
-    face << 2 | vert & 0x3
+    let mut mesh = Mesh { position, normal, texcoord, tangent: Vec::new() };
+    mikktspace::generate_tangents(&mut mesh);
+    return mesh.tangent; // TODO(cleanup): Clearer success/failure signal?
 }
 
 /******************************************************************************
- * Bindings.
+ * MikkTSpace library interface.
  */
 
 #[wasm_bindgen]
@@ -134,30 +75,13 @@ impl Geometry for Mesh {
     }
 }
 
-/// Generates tangents for the input geometry.
-///
-/// # Errors
-///
-/// Returns `false` if the geometry is unsuitable for tangent generation including,
-/// but not limited to, lack of vertices.
-#[wasm_bindgen]
-#[allow(non_snake_case)]
-pub fn computeVertexTangents(position: Vec<f32>, normal: Vec<f32>, texcoord: Vec<f32>) -> Vec<f32> {
-    // panic::set_hook(Box::new(console_error_panic_hook::hook));
-    let mut mesh = Mesh { position, normal, texcoord, tangent: Vec::new() };
-    unsafe { mikktspace::genTangSpace(&mut mesh, 180.0) };
-    return mesh.tangent; // TODO(cleanup): Clearer success/failure signal?
-}
-
 /******************************************************************************
- * Debug.
+ * Debugging.
  */
 
 #[cfg(debug_assertions)]
 #[wasm_bindgen]
 extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 }
